eaafa207e5abc0f6d71575cceb6f824a73369588
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2020-04-25 23:20:06 +0300

    Init

---
+++ HopfieldNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HopfieldNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
