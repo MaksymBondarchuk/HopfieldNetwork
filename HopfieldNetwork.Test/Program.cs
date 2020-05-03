572988e1b14353ffc9a14f2da52bfd7f64b24085
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2020-05-03 23:58:30 +0300

    10 networks per test

--- HopfieldNetwork.Test/Program.cs
+++ HopfieldNetwork.Test/Program.cs
@@ -7,60 +7,63 @@ namespace HopfieldNetwork.Test
 {
 	internal static class Program
 	{
-		private const int NoiseImagesPerTestSet = 20; 
-		
+		private const int NoiseImagesPerTestSet = 20;
+		private const int NetworksNumber = 10;
+
 		private static void Main()
 		{
 			var noiseGenerator = new NoiseGenerator();
+
+			var testSets = new List<TestSet>
+			{
+				new TestSet
+				{
+					Name = "T",
+					Image = ImagesHelper.T
+				},
+				new TestSet
+				{
+					Name = "K",
+					Image = ImagesHelper.K
+				},
+				new TestSet
+				{
+					Name = "V",
+					Image = ImagesHelper.V
+				},
+				new TestSet
+				{
+					Name = "C",
+					Image = ImagesHelper.C
+				}
+			};
+
+			var networks = new List<Network>(NetworksNumber);
+			for (int i = 0; i < NetworksNumber + 2; i++)
+			{
+				var network = new Network();
+				network.Learn(testSets.Select(s => s.Image).ToList());
+				networks.Add(network);
+			}
+
+			networks = networks.Skip(1).Take(NetworksNumber).ToList();
+
 			for (int noiseLevel = 0; noiseLevel <= 18; noiseLevel++)
 			{
 				// Setup
-				var testSets = new List<TestSet>
-				{
-					new TestSet
-					{
-						Name = "T",
-						Image = ImagesHelper.T,
-						NoiseImages = noiseGenerator.GenerateNoiseImages(ImagesHelper.T, noiseLevel, NoiseImagesPerTestSet)
-					},
-					new TestSet
-					{
-						Name = "K",
-						Image = ImagesHelper.K,
-						NoiseImages = noiseGenerator.GenerateNoiseImages(ImagesHelper.K, noiseLevel, NoiseImagesPerTestSet)
-					},
-					new TestSet
-					{
-						Name = "V",
-						Image = ImagesHelper.V,
-						NoiseImages = noiseGenerator.GenerateNoiseImages(ImagesHelper.V, noiseLevel, NoiseImagesPerTestSet)
-					},
-					new TestSet
-					{
-						Name = "C",
-						Image = ImagesHelper.C,
-						NoiseImages = noiseGenerator.GenerateNoiseImages(ImagesHelper.C, noiseLevel, NoiseImagesPerTestSet)
-					}
-				};
-				
-				// Act
-				var network = new Network();
-				network.Learn(testSets.Select(s => s.Image).ToList());
-
-				// Assert
 				foreach (TestSet testSet in testSets)
 				{
-					// Console.WriteLine(testSet.Name);
-					int rememberedNumber = 0;
-					foreach (List<int> noiseImage in testSet.NoiseImages)
-					{
-						List<int> rememberedImage = network.TryToRemember(noiseImage);
-						if (rememberedImage.SequenceEqual(testSet.Image))
-						{
-							rememberedNumber++;
-						}	
-					}
-					Console.WriteLine($"Noise {noiseLevel,2}. Image {testSet.Name}. Remembered {rememberedNumber} of {NoiseImagesPerTestSet}");
+					testSet.NoiseImages = noiseGenerator.GenerateNoiseImages(testSet.Image, noiseLevel, NoiseImagesPerTestSet);
+				}
+
+				// Act, Assert
+				foreach (TestSet testSet in testSets)
+				{
+					int rememberedNumberAverage = networks
+						.Sum(network => testSet.NoiseImages.Select(network.TryToRemember)
+						.Count(rememberedImage => rememberedImage.SequenceEqual(testSet.Image))) / NetworksNumber;
+
+					Console.WriteLine($"Noise {noiseLevel,2}. Image {testSet.Name}. Remembered {rememberedNumberAverage} of {NoiseImagesPerTestSet}");
 				}
 			}
 		}
